31fb70a9d97d014ca5e7410ac5e0ddd0e455cb42
Michael Lemmon <lemmon04@yahoo.com>
2017-07-31 15:23:59 -0700

    Create Copy.py

---
+++ Copy.py
@@ -0,0 +1,26 @@
+import arcpy, os
+from arcpy import env
+
+env.workspace = "C:\Users\mlemmon\AppData\Roaming\ESRI\Desktop10.3\ArcCatalog\ESRIPROD gisdbsrv.sde"
+outputFGD = "C:\Users\mlemmon\Documents\ArcGIS\ESRIPRODD_Copy.gdb"
+
+features = arcpy.ListFeatureClasses()
+for feature in features:
+    feature_split = feature.split(".")
+    schema = feature_split[1]
+    name = feature_split[2]
+    try:
+        arcpy.CopyFeatures_management(feature,outputFGD + "/" + schema + "_" + name)
+    except:
+        print feature
+
+tables = arcpy.ListTables()
+for table in tables:
+    table_split = table.split(".")
+    schema_table = table_split[1]
+    name_table = table_split[2]
+    try:
+        arcpy.Copy_management(table,outputFGD + "/" + schema_table + "_" + name_table)
+    except:
+        print table
+    
